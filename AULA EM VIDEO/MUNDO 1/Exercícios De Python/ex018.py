14b5ea5bdeeb9186f27a6f62f56664df65b2ea2a
Denner Oliveira (Tota) <78981303+DehOliver@users.noreply.github.com>
2025-01-23 01:18:27 -0300

    Lesson modify

--- AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex018.py
+++ AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex018.py
@@ -1,8 +1,8 @@
 import math
 
 num_1 = float(input("Digite o Valor do ângulo que deseja calcular: "))
-seno = math.sin(num_1)
-cosseno = math.cos(num_1)
-tangente = math.tan(num_1)
+seno = math.sin(math.radians(num_1))
+cosseno = math.cos(math.radians(num_1))
+tangente = math.tan(math.radians(num_1))
 
-print("O Ângulo de {} tem o Seno de {}°, o Cosseno de {}° e a Tangente de {}°.".format(num_1, seno, cosseno, tangente))
+print("O Ângulo de {} tem o Seno de {:.2f}°, o Cosseno de {:.2f}° e a Tangente de {:.2f}°.".format(num_1, seno, cosseno, tangente))
