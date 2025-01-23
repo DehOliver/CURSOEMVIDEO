620e2171342e268fe68642604a168730aebdd515
Denner Oliveira (Tota) <78981303+DehOliver@users.noreply.github.com>
2025-01-23 01:26:38 -0300

    saving

---
+++ AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex019.py
@@ -0,0 +1,9 @@
+import random
+
+al1 = str(input("Qual o nome do primeiro aluno? "))
+al2 = str(input("Qual o nome do segundo aluno? "))
+al3 = str(input("Qual o nome do terceiro aluno? "))
+al4 = str(input("Qual o nome do quarto aluno? "))
+escolhido = random.choice(al1, al2, al3, al4)
+
+print("O aluno escolhido para a tarefa foi {}.".format(escolhido))
