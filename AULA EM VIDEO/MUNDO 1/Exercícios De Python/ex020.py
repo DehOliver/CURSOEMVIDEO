4103f479e64ebb6f75a459c520d5d08cecc79f74
Denner Oliveira (Tota) <denner.florescente@gmail.com>
2025-01-23 19:16:58 -0300

    ex020

--- AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex020.py
+++ AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex020.py
@@ -6,6 +6,6 @@ al2 = str(input("Quem é o segundo aluno? "))
 al3 = str(input("Quem é o terceiro aluno? "))
 al4 = str(input("Quem é o quarto aluno? "))
 lista = [al1, al2, al3, al4]
-ordem = shuffle(lista)
+shuffle(lista)
 
-print("A ordem de apresentação será:\n{}".format(ordem))
+print("A ordem de apresentação será:\n{}".format(lista))
