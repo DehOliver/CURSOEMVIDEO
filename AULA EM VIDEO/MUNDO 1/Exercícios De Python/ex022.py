3ad915e69d729df6d0806f805e7b76c783e9dcd6
Denner Oliveira (Tota) <denner.florescente@gmail.com>
2025-01-24 16:31:39 -0300

    ex022

---
+++ AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex022.py
@@ -0,0 +1,5 @@
+name = str(input("Digite seu nome completo: "))
+
+print("Seu nome em maiúsculas é {}.".format(name.upper()))
+print("Seu nome em minusculas é {}.".format(name.lower()))
+print("Seu nome tem ao todo {} letras.".format())
