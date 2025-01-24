85e7bccc4a171d892514d463f66dbee849ea62ff
Denner Oliveira (Tota) <denner.florescente@gmail.com>
2025-01-24 17:03:07 -0300

    ex022

--- AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex022.py
+++ AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex022.py
@@ -1,7 +1,7 @@
-name = str(input("Digite seu nome completo: "))
+name = str(input("Digite seu nome completo: ")).strip()
 divide_name = name.split()
 
 print("Seu nome em maiúsculas é {}.".format(name.upper()))
 print("Seu nome em minusculas é {}.".format(name.lower()))
-print("Seu nome tem ao todo {} letras.".format(len(name)))
+print("Seu nome tem ao todo {} letras.".format(len(name) - name.count(" ")))
 print("Seu primeiro nome é {} e ele tem {} letras.".format(divide_name[0], len(divide_name[0])))
