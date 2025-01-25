cf85cdbb8a19b555edea34275f0b317faa5a9885
Denner Oliveira (Tota) <denner.florescente@gmail.com>
2025-01-25 02:45:24 -0300

    ex027

---
+++ AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex027.py
@@ -0,0 +1,5 @@
+name = str(input("Digite seu nome completo: ")).strip()
+divide = name.split()
+print("Muito prazer em te conhecer!")
+print("Seu primeiro nome é {}.".format(divide[0]))
+print("Seu último nome é {}.".format(divide[len(divide)-1]))
