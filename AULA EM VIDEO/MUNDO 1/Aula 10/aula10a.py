d9ed3307d57f5e49c1b6228d04e697d22670baae
Denner Oliveira (Tota) <denner.florescente@gmail.com>
2025-01-25 15:55:45 -0300

    aula 10a 0.0.3

--- AULA EM VIDEO/MUNDO 1/Aula 10/aula10a.py
+++ AULA EM VIDEO/MUNDO 1/Aula 10/aula10a.py
@@ -1,4 +1,6 @@
 nome = str(input("Qual é o seu nome? "))
 if nome == "Denner":
     print("Que nomezinho dificil, hein menino!")
+else:
+    print("Seu nome é simples, graças a Zeus!")
 print("Buenas seras, {}!".format(nome))
