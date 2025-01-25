089344dd06f711b29532ac2e2a06a7774cc744c7
Denner Oliveira (Tota) <denner.florescente@gmail.com>
2025-01-25 16:03:54 -0300

    aula 10a 0.0.4

--- AULA EM VIDEO/MUNDO 1/Aula 10/aula10a.py
+++ AULA EM VIDEO/MUNDO 1/Aula 10/aula10a.py
@@ -1,6 +1,13 @@
-nome = str(input("Qual é o seu nome? "))
+"""nome = str(input("Qual é o seu nome? "))
 if nome == "Denner":
     print("Que nomezinho dificil, hein menino!")
 else:
     print("Seu nome é simples, graças a Zeus!")
-print("Buenas seras, {}!".format(nome))
+print("Buenas seras, {}!".format(nome))"""
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+media = (n1 + n2 + n3)/ 3
+
+print("As notas foram {:.1f}, {:.1f} e {:.1f} e a média é {:.2f}.".format(n1, n2, n3, media))
