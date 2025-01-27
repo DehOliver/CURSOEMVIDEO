e8ee1858a7506b6f7d751cf8fc07ffa78abf5be9
Denner Oliveira (Tota) <denner.florescente@gmail.com>
2025-01-26 21:39:56 -0300

    ex028 0.0.3

--- AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex028.py
+++ AULA EM VIDEO/MUNDO 1/Exercícios De Python/ex028.py
@@ -1,7 +1,8 @@
 import random
 import time
-
+print("-=-" * 20)
 print("Escolhendo um número entre 0 e 10...")
+print("-=-" * 20)
 random_number = random.randint(0, 10)
 time.sleep(2)
 
@@ -9,5 +10,5 @@ user_choice = print(input("O número foi escolhido! Tente acertar minha decisão
 if user_choice == random_number:
     print("Parabéns! Você acertou! (Não trapaceou né?)")
 else:
-    print("Você errou! O número, agora eu sou o dono deste dispositivo!")   
+    print("Você errou! O número era {}, agora eu sou o dono deste dispositivo!".format(random_number))   
     
